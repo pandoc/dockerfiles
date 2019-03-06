5968e0bbe17bae91006181fc6aad66e554796436
Stephen McDowell <svenevs.dev@gmail.com>
2019-03-06 08:17:15 -0800

    install missing packages, modularize for cache benefits
    - install missing packages as identified by tests
    - install packages for graphicx, grffile, and longtable
    - move tlmgr package installs to separate script so that
      install-tl does not rerun every time.
    - make package listings alphabetical for consistency

--- latex-common/install-texlive.sh
+++ latex-common/install-texlive.sh
@@ -1,66 +1,12 @@
 #!/bin/sh
 
-################################################################################
-# Download / extract the install-tl perl script.                               #
-################################################################################
+# Download / extract the install-tl perl script.
 wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz || exit 1
 mkdir -p ./install-tl
 tar --strip-components 1 -zvxf install-tl-unx.tar.gz -C "$PWD/install-tl" || exit 1
 
-################################################################################
-# Run the default installation with the specified profile.                     #
-################################################################################
+# Run the default installation with the specified profile.
 ./install-tl/install-tl --profile=/root/texlive.profile
 
-################################################################################
-# Install pandoc latex pacakges: https://pandoc.org/MANUAL.html#creating-a-pdf #
-################################################################################
-# NOTE: graphicx, grffile, and longtable appear to be default packages?  tlmgr
-#       cannot install them and will error out. That is why they are excluded.
-tlmgr install amsfonts     \
-              amsmath      \
-              lm           \
-              lm-math      \
-              unicode-math \
-              ifxetex      \
-              ifluatex     \
-              listings     \
-              fancyvrb     \
-              booktabs     \
-              hyperref     \
-              xcolor       \
-              ulem         \
-              geometry     \
-              setspace     \
-              babel
-
-################################################################################
-# Install extra packages for XeTex, LuaTex, and BibLaTex.                      #
-################################################################################
-tlmgr install xetex       \
-              luatex      \
-              fontspec    \
-              polyglossia \
-              xecjk       \
-              bidi        \
-              mathspec    \
-              upquote     \
-              microtype   \
-              csquotes    \
-              lualatex-math
-
-# Make sure all reference backend options are installed
-tlmgr install natbib   \
-              biblatex \
-              bibtex   \
-              biber
-
-################################################################################
-# Trim down (possibly large amounts of) installed artifacts such as docs.      #
-################################################################################
-rm -rf ./install-tl                        \
-       ./install-tl-unx.tar.gz             \
-       /opt/texlive/texdir/texmf-dist/doc  \
-       /opt/texlive/texdir/readme-html.dir \
-       /opt/texlive/texdir/readme-txt.dir  \
-       /opt/texlive/texdir/install-tl*
+# Cleanup installation artifacts.
+rm -rf ./install-tl ./install-tl-unx.tar.gz
