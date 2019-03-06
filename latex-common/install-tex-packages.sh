98c0f9d863008dfe545c7876d75f072548b4fbdb
Stephen McDowell <svenevs.dev@gmail.com>
2019-03-06 08:17:15 -0800

    install missing packages, modularize for cache benefits
    - install missing packages as identified by tests
    - install packages for graphicx, grffile, and longtable
    - move tlmgr package installs to separate script so that
      install-tl does not rerun every time.
    - make package listings alphabetical for consistency

---
+++ latex-common/install-tex-packages.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+################################################################################
+# Install pandoc latex pacakges: https://pandoc.org/MANUAL.html#creating-a-pdf #
+################################################################################
+# NOTE: search left hand side on CTAN to see for yourself:
+#       graphicx  -> graphics
+#       grffile   -> oberdiek
+#       longtable -> tools
+tlmgr install amsfonts \
+              amsmath \
+              babel \
+              booktabs \
+              fancyvrb \
+              geometry \
+              graphics \
+              hyperref \
+              ifluatex \
+              ifxetex \
+              listings \
+              lm \
+              lm-math \
+              oberdiek \
+              setspace \
+              tools \
+              ulem \
+              unicode-math \
+              xcolor \
+              || exit 1
+
+################################################################################
+# Install extra packages for XeTex, LuaTex, and BibLaTex.                      #
+################################################################################
+tlmgr install bidi \
+              csquotes \
+              fontspec \
+              luatex \
+              lualatex-math \
+              mathspec \
+              microtype \
+              polyglossia \
+              upquote \
+              xecjk \
+              xetex \
+              || exit 1
+
+# Make sure all reference backend options are installed
+tlmgr install biber \
+              biblatex \
+              bibtex \
+              natbib \
+              || exit 1
+
+# These packages were identified by the tests, they are likely dependencies of
+# dependencies that are not encoded well.
+tlmgr install footnotehyper \
+              xurl \
+              || exit 1
+
+################################################################################
+# Trim down (possibly large amounts of) installed artifacts such as docs.      #
+################################################################################
+rm -rf /opt/texlive/texdir/texmf-dist/doc  \
+       /opt/texlive/texdir/readme-html.dir \
+       /opt/texlive/texdir/readme-txt.dir  \
+       /opt/texlive/texdir/install-tl*
