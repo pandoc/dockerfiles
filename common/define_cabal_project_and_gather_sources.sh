2ae8fe71f99011a66a93e1b4b9d3dbae172c19cd
Stephen McDowell <svenevs.dev@gmail.com>
2019-10-20 00:56:02 -0400

    ok so we have to patch all of them.  check release=2.7.3

--- common/define_cabal_project_and_gather_sources.sh
+++ common/define_cabal_project_and_gather_sources.sh
@@ -25,6 +25,54 @@ cd "$old_pwd"
 # >>>>>>>>>>>>> v slash required for cabal to use the folder!
 echo "    pandoc/" >> /usr/src/cabal.project
 
+################################################################################
+# HACK:
+# LaTeX 2019 need to bypass: https://github.com/jgm/pandoc/issues/5801
+# We will re-release 2.6 ... 2.7.3 to add pandoc-crossref, this code will not
+# be needed forever.
+#
+# Crude version detection: always make sure to check != $pandoc_commit!
+major="$(echo "$pandoc_commit" | sed 's/^\([0-9]\)\..*$/\1/')"
+minor="$(echo "$pandoc_commit" | sed 's/^[0-9]\.\([0-9]\).*$/\1/')"
+patch="$(echo "$pandoc_commit" | sed 's/^[0-9]\.[0-9]\.\([0-9]\)$/\1/')"
+do_patch=0
+if ! [ "$major" = "$pandoc_commit" ]; then
+    if [ "$major" = "2" ]; then
+        if ! [ "$minor" = "$pandoc_commit" ]; then
+            if [ "$minor" -le 7 ]; then
+                if [ "$patch" = "$pandoc_commit" ]; then
+                    do_patch=1
+                elif [ "$patch" -le 3 ]; then
+                    do_patch=1
+                fi
+            fi
+        fi
+    fi
+fi
+# https://github.com/jgm/pandoc/commit/d9db76dcf40d1930ad15317d76fd2c90d9114801
+# Patch does not apply cleanly to all versions needing patching, just manually
+# patch the relevant section of the writer.
+if [ "$do_patch" = "1" ]; then
+    cd /usr/src/pandoc
+    src='\\\\begin{center}\\\\rule{0\.5\\\\linewidth}{\\\\linethickness}\\\\end{center}'
+    dst='\\begin{center}\\rule{0.5\\linewidth}{0.5pt}\\end{center}'
+    sed "s/$src/$dst/g" < src/Text/Pandoc/Writers/LaTeX.hs > tmp.hs
+    mv tmp.hs src/Text/Pandoc/Writers/LaTeX.hs
+    # Display patched changes.
+    git --no-pager diff --color=always
+    # Make sure to exit if no patching was done!
+    if git diff --quiet; then
+        echo 'NO PATCH PERFORMED BUT PATCH EXPECTED!'
+        exit 1
+    fi
+    cd "$old_pwd"
+fi
+
+
+
+# /HACK
+################################################################################
+
 # 3. Clone specific branch of pandoc-crossref if non-release-tag build.
 #    Otherwise, use hackage to obtain for stable releases.  Does not work for
 #    branches with `.` in them, `pandoc` has few of these though.
