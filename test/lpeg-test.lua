54bf69189bca1cdbfc834cda4b79d6e63542639e
Albert Krewinkel <albert@zeitkraut.de>
2022-01-05 21:25:31 +0100

    Add "static" stack: images contain only a statically compiled binary

--- test/lpeg-test.lua
+++ test/lpeg-test.lua
@@ -1,7 +1,11 @@
 if PANDOC_VERSION < '2.16.2' then
   local lpeg = require 'lpeg'
 else
-  assert(lpeg and lpeg == require 'lpeg', 'lpeg not loaded from system lib')
+  -- Hacky way to test if we are in the static image, as the assertion is
+  -- expected to fail lpeg was compiled into pandoc.
+  if os.getenv('HOME') ~= '/' then
+    assert(lpeg and lpeg == require 'lpeg', 'lpeg not loaded from system lib')
+  end
 end
 if lpeg.type(lpeg.P 'Hello') ~= 'pattern' then
   io.stderr:write('[ERROR] LPEG not working as expected\n')
