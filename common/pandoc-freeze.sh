3aa890001eeb4f5b68234ccf5a569fe7a4dab3e2
Albert Krewinkel <albert@zeitkraut.de>
2022-01-06 13:38:23 +0100

    pandoc-freeze.sh: cleanup, allow to pass the build stack

--- common/pandoc-freeze.sh
+++ common/pandoc-freeze.sh
@@ -3,31 +3,70 @@
 ## Haskell dependencies to specific version, improving our ability
 ## to do reproducible builds.
 ##
-## Expected inputs:
-##   1. pandoc version, e.g. 2.9.2.1
-##   2. owner of the new freeze file, e.g. `user:group` or
-##      `1000:1000`.
-##
 ## Files created by this script should be put under version control.
 set -e
 
 usage ()
 {
     printf "Generate a cabal freeze file for a given pandoc version\n\n"
-    printf "Usage: %s PANDOC_COMMIT FILE_OWNER\n\n" "$0"
-    printf "Parameters:\n"
-    printf "  PANDOC_COMMIT: targeted pandoc commit, e.g. 2.9.2.1\n"
-    printf "  FILE_OWNER: owner of the new freeze file, e.g. 1000:1000\n\n"
-    printf "  TARGET_FILE: target file name\n"
-    printf "NOTE: This script is designed to run in a Docker container. The\n"
-    printf "freeze file will be created in the '/app' directory.\n"
+    printf 'Usage: %s %s\n\n' \
+           "$0" \
+           '[-v] [-c pandoc_commit] [-s stack] [-u user] [-o outfile]'
+    printf 'Parameters:\n'
+    printf '  -v: increase verbosity\n'
+    printf '  -c pandoc_commit: targeted pandoc commit, e.g. 2.9.2.1\n'
+    printf '  -s stack: the stack for which the freeze file is built\n'
+    printf '  -u user: owner of the new freeze file, e.g. 1000:1000\n'
+    printf '  -o outfile: target file name\n\n'
+    printf 'NOTE: This script is designed to run in a Docker container.\n'
 }
-# Bail unless the script is called with exactly two parameters
-[ $# -eq 3 ] || ( usage 1>&2; exit 1 )
 
-pandoc_commit="$1"
-file_owner="$2"
-target_file="$3"
+if ! args=$(getopt 's:c:o:u:v' "$@"); then
+    usage && exit 1
+fi
+# The variable is intentionally left unquoted.
+# shellcheck disable=SC2086
+set -- $args
+
+stack=unknown
+pandoc_commit=master
+user=$(id -u):$(id -g)
+outfile=cabal.project.freeze
+verbosity=0
+
+while true; do
+    case "$1" in
+        (-c)
+            pandoc_commit="${2}";
+            shift 2
+            ;;
+        (-o)
+            outfile="${2}";
+            shift 2
+            ;;
+        (-s)
+            stack="${2}"
+            shift 2
+            ;;
+        (-u)
+            user="${2}"
+            shift 2
+            ;;
+        (-v)
+            verbosity=$((verbosity + 1));
+            shift 1
+            ;;
+        (--) shift; break;;
+        (*) usage; exit 1;;
+    esac
+done
+
+if [ "$verbosity" -gt 0 ]; then
+    printf 'stack: %s\n' "${stack}"
+    printf 'pandoc_commit: %s\n' "${pandoc_commit}"
+    printf 'user: %s\n' "${user}"
+    printf 'outfile: %s\n' "${outfile}"
+fi
 
 tmpdir=$(mktemp -p /tmp -d pandoc-freeze.XXXXXX)
 cd "${tmpdir}"
@@ -69,9 +108,9 @@ print_constraints_only ()
 # Just write the constraints to the target file when targeting master
 if [ "$pandoc_commit" = "master" ]; then
     printf "Writing freeze file for builds from master...\n"
-    print_constraints_only > "${target_file}"
-    printf "Changing freeze file owner to %s\n" "${file_owner}"
-    chown "${file_owner}" "${target_file}"
+    print_constraints_only > "${outfile}"
+    printf "Changing freeze file owner to %s\n" "${user}"
+    chown "${user}" "${outfile}"
     exit 0
 fi
 
@@ -98,16 +137,16 @@ cabal v2-freeze \
       --constraint="lpeg ${lpeg_constraints}" \
       --constraint="${lua_package} ${lua_constraints}"
 
-printf "Copying freeze file to %s\n" "${target_file}"
-target_dir="$(dirname "${target_file}")"
+printf "Copying freeze file to %s\n" "${outfile}"
+target_dir="$(dirname "${outfile}")"
 if [ ! -d "${target_dir}" ]; then
     mkdir -p "${target_dir}"
     chmod 775 "${target_dir}"
-    chown "${file_owner}" "${target_dir}"
+    chown "${user}" "${target_dir}"
 fi
-cp cabal.project.freeze "${target_file}"
-printf "Changing freeze file owner to %s\n" "${file_owner}"
-chmod 664 "${target_file}"
-chown "${file_owner}" "${target_file}"
+cp cabal.project.freeze "${outfile}"
+printf "Changing freeze file owner to %s\n" "${user}"
+chmod 664 "${outfile}"
+chown "${user}" "${outfile}"
 
 printf "DONE\n"
