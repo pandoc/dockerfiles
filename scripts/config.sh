78d575c7d53f33d99439b14e95adddfc543dc66c
Albert Krewinkel <albert@zeitkraut.de>
2024-10-20 14:06:53 +0200

    config: fix linting warnings

--- scripts/config.sh
+++ scripts/config.sh
@@ -19,7 +19,7 @@ versions_file=versions.md
 row="$(grep "^| $version " "$versions_file")"
 
 if [ -z "$row" ]; then
-    printf "Unknown or unsupported version '%s'\n" "$version"
+    printf 'Unknown or unsupported version "%s"\n' "$version"
     exit 1
 fi
 
@@ -41,7 +41,7 @@ case "${build_stack}" in
         base_image_version="$(field 4)"
         ;;
     (*)
-        printf "Unsupported base image: '%s'\n" "$base_image"
+        printf 'Unsupported base image: "%s"\n' "$base_image"
         ;;
 esac
 
@@ -49,15 +49,15 @@ version_tags="$(field 2)"
 tags=
 for name in $(printf '%s\n' "$images" | tr ',' ' '); do
     image_tags_base="$( \
-        printf "$version_tags" | sed -e "s#\([^,]*\)#${name}:\1#g" \
+        printf '%s\n' "$version_tags" | sed -e "s#\([^,]*\)#${name}:\1#g" \
     )"
     image_tags="$(printf '%s' "$image_tags_base" | \
                     sed -e 's#\([^,]*\)#\1-'${base_image}'#g')"
-    image_name=$(printf "$name" | sed -e 's#.*\([^/]\+/[^/]\+\)$#\1#')
-    if ( [ "$image_name" != 'pandoc/minimal' ] && \
-         [ "$build_stack" = 'alpine' ] ) ||
-       ( [ "$image_name"  = 'pandoc/minimal' ] && \
-         [ "$build_stack" = 'static' ] );
+    image_name=$(printf '%s\n' "$name" | sed -e 's#.*\([^/]\+/[^/]\+\)$#\1#')
+    if { [ "$image_name" != 'pandoc/minimal' ] && \
+         [ "$build_stack" = 'alpine' ]; } ||
+       { [ "$image_name"  = 'pandoc/minimal' ] && \
+         [ "$build_stack" = 'static' ]; };
     then
         tags="${tags},${image_tags_base},${image_tags}"
     else
