e3da110f55fee1127002c244ae751991e38c61b0
Albert Krewinkel <albert@zeitkraut.de>
2025-03-11 11:43:29 +0100

    Bump default TeXLive version to 2025

--- common/latex/install-texlive.sh
+++ common/latex/install-texlive.sh
@@ -2,7 +2,7 @@
 
 # NOTE TO MAINTAINERS: this must be updated each time a new texlive is
 # released!
-default_version=2024
+default_version=2025
 tlversion=${1:-"$default_version"}
 installer_archive=install-tl-unx.tar.gz
 
