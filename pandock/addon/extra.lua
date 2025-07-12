2af4dc378e35bf84ee75831db626438807c81ba4
Albert Krewinkel <albert@zeitkraut.de>
2025-07-12 13:26:37 +0200

    Include extra parameters in template context

--- pandock/addon/extra.lua
+++ pandock/addon/extra.lua
@@ -6,8 +6,27 @@
 local List   = require 'pandoc.List'
 local system = require 'pandock.system'
 
+local extra_packages_filepath = 'common/extra/packages.txt'
+local extra_python_requirements = 'common/extra/requirements.txt'
+
+local function get_packages (filepath)
+  local contents = system.read_file(filepath)
+  local packages = List()
+  for line in contents:gmatch('[^\n]+') do
+    packages:insert((line:gsub('%s*%#.*', '')))
+  end
+  packages:sort()
+  return packages:filter(function (pkg)
+      return #pkg > 0
+  end)
+end
+
 local function addon_context (extra, release)
-  return release.addon.extra
+  return {
+    ['packages'] = get_packages(extra_packages_filepath),
+    ['eisvogel'] = release.addon.extra.eisvogel,
+    ['python']   = release.addon.extra.python
+  }
 end
 
 return {
