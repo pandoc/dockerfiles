894e9e217f3d7ca28a1ceadbb30a0053f7bbbabd
Albert Krewinkel <albert@zeitkraut.de>
2025-07-12 10:02:43 +0200

    Make bake files relative to the directory that they are in

--- pandock/bakefile.lua
+++ pandock/bakefile.lua
@@ -6,14 +6,20 @@
 local pandoc      = require 'pandoc'
 local List        = require 'pandoc.List'
 local json        = require 'pandoc.json'
+local path        = require 'pandoc.path'
 local BuildTarget = require 'pandock.type.BuildTarget'
 
 --- Bakefile module
 local bakefile = {}
 
 local function make_bake_target (build_target)
+  -- Get the path to the dockerfile, relative to the bake file.
+  local dockerfile_path = build_target:dockerfile_filepath()
+  local dockerfile_path_components = List(path.split(dockerfile_path))
+  dockerfile_path_components:remove(1)
+  local dockerfile_relative_path = path.join(dockerfile_path_components)
   return {
-    dockerfile = build_target:dockerfile_filepath(),
+    dockerfile = dockerfile_relative_path,
     labels = build_target:labels(),
     tags = build_target:tags(),
     target = build_target:target(),
