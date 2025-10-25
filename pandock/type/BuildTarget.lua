d4c39c369cec413279701c4640178f30c46e4098
Albert Krewinkel <albert@zeitkraut.de>
2025-10-25 12:01:11 +0200

    Fix bakefile generation

--- pandock/type/BuildTarget.lua
+++ pandock/type/BuildTarget.lua
@@ -96,7 +96,8 @@ BuildTarget.targets_for_release = function (release, variants)
     -- sort to get a fixed, reproducible order
     base_images:sort()
     local supports_variant = function (base_image)
-      return release.addon[variant] and base_image ~= 'static'
+      return (release.addon[variant.name]
+              and base_image ~= 'static')
         or not variant:is_addon()
     end
     targets:extend(
